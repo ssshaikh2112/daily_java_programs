45727f6d84c0d708d9da34e6c38efa13b3738a74
Sahil Shaikh <105914225+SSShaikh07@users.noreply.github.com>
2024-02-14 21:28:42 +0530

    Create ForLoopWithoutInti.java

---
+++ ForLoopWithoutInti.java
@@ -0,0 +1,11 @@
+class ForLoopWithoutInti
+  {
+    public static void main(String args[]) 
+    {
+      int i = 1;
+      for( ; i<=5; i++)
+        {
+          System.out.println(i);
+        }
+    }
+  }
