d847f53caf7e32ff049907579203f9125b1fdade
Sahil Shaikh <105914225+SSShaikh07@users.noreply.github.com>
2024-02-15 11:22:26 +0530

    Add files via upload

---
+++ NumFromFiveToOne.java
@@ -0,0 +1,14 @@
+class NumFromFiveToOne
+{
+	public static void main(String args[])
+	{
+	
+		// Print numbers from 5 to 1 in desceding order...
+
+		for(int i=5;i>=1;i--)		// number from 5 to 1
+		{
+			System.out.println(i);	// print number
+		}
+	}
+
+}
