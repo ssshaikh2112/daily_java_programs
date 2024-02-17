8b655c12045975d1ad09cacd675c95d8cf4f7c98
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-17 20:10:31 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+class EvenOdd
+{
+	public static void main(String args[])
+	{
+		int number = 8;
+
+		if(number%2==0)
+		{
+			System.out.println("The number is even : "+number);
+		}
+		else
+		{
+			System.out.println("The number is odd : "+number);
+		}
+	}
+}
