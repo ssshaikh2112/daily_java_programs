d899a34c123523ba0b06a7a72fc4f1b12881034c
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-18 17:34:33 +0530

    Add files via upload

---
+++ DefaultValueOfVar.java
@@ -0,0 +1,27 @@
+class DefaultValueOfVar
+{
+	
+	// Static variables are print the default values after run These are below.
+
+
+	static int variableOne;
+	static char variableTwo;
+	static byte variableThree;
+	static float variableFour;
+	static boolean variableFive;
+	static double variableSix;
+	static short variableSeven;
+	static long variableEight;
+	public static void main(String args[])
+	{
+		System.out.println("The Default value of int is : "+variableOne);			// 0
+		System.out.println("The Default value of char is : "+variableTwo);			// blank ( )
+		System.out.println("The Default value of byte is : "+variableThree);		//0
+		System.out.println("The Default value of float is : "+variableFour);		//0.0
+		System.out.println("The Default value of boolean is : "+variableFive);		//false
+		System.out.println("The Default value of double is : "+variableSix);		//0.0
+		System.out.println("The Default value of short is : "+variableSeven);		//0
+		System.out.println("The Default value of long is : "+variableEight);		//0
+
+	}
+}
