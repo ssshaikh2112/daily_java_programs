419b2dc57a1590c882f7ae72e5f511a553874839
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-18 17:16:44 +0530

    Add files via upload

---
+++ TypesOfAccessVar.java
@@ -0,0 +1,19 @@
+class TypesOfAccessVar
+{
+	
+	static int number = 5;
+	public static void main(String args[])
+	{
+		// There are the 3 types of access of the variable...
+		// 1.direct access	 2.class_name	 3.object_creation
+
+	
+			System.out.println("These is the direct access type , number is : "+number);	//1.direct access
+
+			System.out.println("These is the class name type of access , and number is : "+TypesOfAccessVar.number);	//2.class_name
+
+			// Object creation we will learn in the class and object section
+
+
+	}
+}
