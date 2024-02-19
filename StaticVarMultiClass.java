c2d1feb8f8508af28a4e6ff72acc0ded62fdd10d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-19 22:32:36 +0530

    Add files via upload

---
+++ StaticVarMultiClass.java
@@ -0,0 +1,13 @@
+class ValueOfChar
+{
+	static char a='s';
+}
+
+
+class StaticVarMultiClass
+{
+	public static void main(String args[])
+	{
+		System.out.println(ValueOfChar.a);		// to access use the class_name.variable_name
+	}
+}
