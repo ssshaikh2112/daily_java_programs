a7287b37331378a11d140786ac9392e3dee3a08f
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-20 11:10:08 +0530

    Add files via upload

---
+++ StarPatternOpposite.java
@@ -0,0 +1,30 @@
+class StarPatternOpposite
+{
+
+	/*
+			*
+		   **
+		  ***
+		 ****
+		*****
+	Print the Above pattern
+		*/
+
+	public static void main(String args[])
+	{
+		for (int column=1;column<=5 ;column++ )
+		{
+			for (int space=4;space >=column;space--)		// to print the space before the * 
+			{
+				System.out.print(" ");
+			}
+
+			for (int row=1;row<=column ;row++ )				// to print the * at last of space
+			{
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+}
