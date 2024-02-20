b0eb2da447d0fd880db076568b0758e642f07e90
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-20 11:38:56 +0530

    Update StarPattern3.java

--- StarPattern3.java
+++ StarPattern3.java
@@ -11,9 +11,9 @@ class StarPattern3
 
 	public static void main(String args[])
 	{
-		for(int column=5;column>=1;column--)
+		for(int column=5;column>=1;column--)  		// column = 5 ,4,3,2,1
 		{
-			for(int row=1;row<=column;row++)
+			for(int row=1;row<=column;row++)	//Print 5 * in first line and then increase value of row means print then 4 (row=1,2,3,4,5)
 			{
 				System.out.print("* ");
 			}
