fddda6185516b65ea8fd5b7ea8ebe29b4081cfeb
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-21 10:48:17 +0530

    Add files via upload

---
+++ StarPattern5.java
@@ -0,0 +1,19 @@
+class StarPattern5
+{
+	public static void main(String args[])
+	{
+		for (int column =5;column>=1  ;column-- )
+		{
+			for (int space=1 ;space<column ;space++ )
+			{
+				System.out.print(" ");
+			}
+
+			for (int row=5 ;row >=column ;row-- )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
