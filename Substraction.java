8b5727297713b36a5a5a766d86c371ee87cc4e0f
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-02-25 19:26:30 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,11 @@
+class Substraction
+{
+	public static void main(String args[])
+	{
+		int number1=12;
+		int number2=10;
+		int result;
+		result=number1-number2;
+		System.out.println(result);
+	}
+}
