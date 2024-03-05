dd533b220796cfc4375b6d0399c9f25a808d75fd
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-05 20:40:53 +0530

    Add files via upload

--- StaticAndNonStatic/NStaVMInNStaArea.java
+++ StaticAndNonStatic/NStaVMInNStaArea.java
@@ -1,14 +1,27 @@
-class NStaVMInNStaArea
+
+class Demo
 {
 	float number=100.5f;
 	void myMethod()
 	{
+		System.out.println();
 		System.out.println("Inside the static method and access by non static area and then main() in multiple classes...");
 	}
+}
 
 
+class NStaVMInNStaArea
+{
+
+	void accessMethod()
+	{
+		Demo obj1 = new Demo();
+		System.out.println(obj1.number);
+		obj1.myMethod();
+	}
 	public static void main(String[] args)
 	{
-		
+		NStaVMInNStaArea obj2 = new NStaVMInNStaArea();
+		obj2.accessMethod();
 	}
 }
