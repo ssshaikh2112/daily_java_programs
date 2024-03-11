d11b167cc1a9c0b5d2ea0ba5e34c537fdf4a6b0f
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-11 22:37:01 +0530

    Add files via upload

---
+++ Inheritance/Example20.java
@@ -0,0 +1,54 @@
+class Demo1
+{
+	Demo1()
+	{
+		// very Constructor may be set super() when will execute the parent constructor And it send the control to the same type of constructor.
+		// Argument object send control to argument constructor 
+		// 0 Argument object send control to 0 argument constructor 
+
+		// super();
+		System.out.println("It is the 0 Constructor of Demo1 -- Demo1");
+	}
+
+	Demo1(int b)
+	{
+		// very Constructor may be set super() when will execute the parent constructor And it send the control to the same type of constructor.
+		// Argument object send control to argument constructor 
+		// 0 Argument object send control to 0 argument constructor 
+
+		// super();
+		System.out.println("It is the 1 Constructor of Demo1 -- Demo1");
+	}
+}
+class Test1 extends Demo1
+{
+	Test1()
+	{
+		// very Constructor may be set super() when will execute the parent constructor And it send the control to the same type of constructor.
+		// Argument object send control to argument constructor 
+		// 0 Argument object send control to 0 argument constructor
+		
+		System.out.println("It is the 0 Constructor of Test1 -- Test1");
+	}
+	Test1(int a)
+	{	
+		// very Constructor may be set super() when will execute the parent constructor And it send the control to the same type of constructor.
+		// Argument object send control to argument constructor 
+		// 0 Argument object send control to 0 argument constructor 
+		
+		super(20);
+		System.out.println("It is the 1 Constructor of Test1 -- Test1");
+	}
+}
+
+/*
+	Every Constructor may be set super() when will execute the parent constructor
+*/
+class Example20
+{
+	public static void main(String args[])
+	{
+		new Test1();
+		new Test1(10);
+	}
+}
