be4162cd96a6fa4b0361fb8e17d67bfbf88eff29
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-13 13:24:39 +0530

    Update Example19.java

--- Inheritance/Example19.java
+++ Inheritance/Example19.java
@@ -17,6 +17,7 @@ class Test1 extends Demo1
 		// very Constructor may be set super() when will execute the parent constructor And it send the control to the same type of constructor.
 		// Argument object send control to argument constructor 
 		// 0 Argument object send control to 0 argument constructor
+
 		
 		// super();
 		System.out.println("It is the 0 Constructor of Test1 -- Test1");
