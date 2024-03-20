3842e4dcba9e5fe7b8f191c24470e514088aad8b
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-20 21:00:33 +0530

    Add files via upload

---
+++ Abstraction/Example1.java
@@ -0,0 +1,18 @@
+abstract class Example1	/*	If class contains abstract method then 
+							by default the class by nature it becomes abstract
+						*/
+{
+	abstract void method();
+		
+	// to make method abstract we use the keyword "abstract"
+	
+}
+
+//whenever we compile normal class or abstract class it will generates .class file.
+
+/*
+
+If class contains incomplete or abstract method such method by nature are abstract or incomplete methods.
+Hence class also abstract by nature	
+
+*/
