111dbeaed348a2807a8d32671b12bf57eeba957e
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-26 06:24:38 +0530

    Add files via upload

---
+++ Polymorphism/Compile Time Poly/Example9.java
@@ -0,0 +1,14 @@
+class Example9
+{
+	/*
+		Java does not support the operator overloading except + operator.
+
+		The + operator has more than one behavior like Addition and Concatination
+	*/
+	public static void main(String args[])
+	{
+		System.out.println(52+65);						// Perform Addition
+		System.out.println("I am "+"Sahil Shaikh");		// Perform Concatination
+		System.out.println("My roll num is : "+101);	// Perform Concatination
+	}
+}
