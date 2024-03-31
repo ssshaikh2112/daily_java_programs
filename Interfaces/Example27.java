3df6f75ba55a26ae15348c2fd940778039bd57e5
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-03-31 12:36:25 +0530

    Add files via upload

---
+++ Interfaces/Example27.java
@@ -0,0 +1,14 @@
+interface It2
+{
+	// check if we can create Constructor
+	// check if we can create Static block
+	// check if we can create Instance Block / Non Static block
+}
+// Assignment 2
+class Example27
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
