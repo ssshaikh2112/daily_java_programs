20ed69529e86d4a6d85b1dd70f795b4e260e0533
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-04-14 15:10:38 +0530

    Update Example27.java

--- Interfaces/Example27.java
+++ Interfaces/Example27.java
@@ -1,11 +1,22 @@
 interface It2
 {
-	// check if we can create Constructor
-	// check if we can create Static block
-	// check if we can create Instance Block / Non Static block
+	// check if we can create Constructor	--> NO
+	It2()
+	{
+		
+	}
+	// check if we can create Static block	-->	NO
+	static
+	{
+		
+	}
+	// check if we can create Instance Block / Non Static block		--> NO
+	{
+		
+	}
 }
 // Assignment 2
-class Example27
+class Example27 implements It2
 {
 	public static void main(String[] args)
 	{
