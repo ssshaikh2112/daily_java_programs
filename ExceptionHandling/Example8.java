b71b238497f3cac679a334f7bf727c7e6337c677
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-04-26 19:40:29 +0530

    Add files via upload

---
+++ ExceptionHandling/Example8.java
@@ -0,0 +1,21 @@
+class Example8
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			System.out.println("These is the try block...");
+			System.out.println(10/2);	
+		}
+		// It is manditory to use the catch block if try block is successfull or not an exception in that.
+		
+	}
+}
+
+/*
+	Example8.java:5: error: 'try' without 'catch', 'finally' or resource declarations
+                try
+                ^
+*/
+
+// It is manditory to use the catch block if try block is successfull or not an exception in that.
