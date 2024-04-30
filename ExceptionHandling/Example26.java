7735696e648cfd2c225dd516b4eabc1b01f8a485
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-04-30 08:26:21 +0530

    Add files via upload

---
+++ ExceptionHandling/Example26.java
@@ -0,0 +1,19 @@
+class Example26
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			System.out.println("Inside the try block");
+
+		}
+		finally
+		{
+			// Finally block will always executed...
+			System.out.println("Inside the Finally block");	
+		}
+	}
+}
+/*
+	try-catch as well as try-finally is also valid.
+*/
