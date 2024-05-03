70d5d300b98ab69560967faac928acc32bc700d1
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-03 16:08:49 +0530

    Add files via upload

---
+++ ExceptionHandling/Example35.java
@@ -0,0 +1,12 @@
+class Example35
+{
+	static void method() throws InterruptedException
+	{
+		Thread.sleep(3000);
+	}
+	public static void main(String[] args) throws InterruptedException
+	{
+		method();
+	}
+}
+//
