1c8fbc09eef5604ae453fdfcec7d6f4100b7dbf7
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-03 16:09:45 +0530

    Add files via upload

---
+++ ExceptionHandling/Example37.java
@@ -0,0 +1,15 @@
+class Example37
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Array");
+		int[] a={10,20,30};
+		int[] b=new int[10000000000000];
+		System.out.println("Array");
+	}
+}
+/*
+	Two type of Error Are present ie.
+	1. OutOfMemoryError				 public java.lang.OutOfMemoryError();
+	2. StackOverFlowError			 public java.lang.StackOverflowError();
+*/
