52fed967a4e543af882a2827df9cd9ef2052068d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-07 07:23:01 +0530

    Add files via upload

---
+++ StringClasses/Example1.java
@@ -0,0 +1,12 @@
+class Example1
+{
+	public static void main(String[] args)
+	{
+		// approach -1 : without new keyword
+		String first="vdf";
+		System.out.println(first);
+		// approach -2 : with new keyword
+		String second = new String("College");
+		System.out.println(second);
+	}
+}
