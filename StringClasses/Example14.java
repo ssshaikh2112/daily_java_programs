ba4e54b74da3cfe019d0675d9872e04c3841fe54
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-10 09:10:22 +0530

    Add files via upload

---
+++ StringClasses/Example14.java
@@ -0,0 +1,11 @@
+class Example14
+{
+	public static void main(String[] args)
+	{
+		String str1 = "Sahil";
+		String str2 = "sahil";
+		// CaseSensitive
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.equals(str2));
+	}
+}
