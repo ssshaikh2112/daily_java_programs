537e12aa31f7410dbe41603dca05342bd10fbbda
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-10 09:10:46 +0530

    Add files via upload

---
+++ StringClasses/Example16.java
@@ -0,0 +1,10 @@
+class Example16
+{
+	// Write a program to calculate length of string
+	// length()
+	public static void main(String[] args)
+	{
+		String str="Sahil";
+		System.out.println(str.length());
+	}
+}
