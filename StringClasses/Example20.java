213c5648d86b4a1ea4193b74bb65b4dc06ccfa2d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-11 11:09:04 +0530

    Add files via upload

---
+++ StringClasses/Example20.java
@@ -0,0 +1,15 @@
+class Example20
+{
+	// Assignment
+	/*
+		Use String method
+		hint--> check the . operator --> .oprator should extact
+		sop()--> only one extension-->xlsx
+		sop()--> only the extension --> doc
+	*/
+	public static void main(String[] args)
+	{
+		String filename="student.xlsx";	//college.doc
+
+	}
+}
