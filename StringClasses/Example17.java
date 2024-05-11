cf8f34a96d2b7751b25d8c0e97bc5248bc20cac0
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-11 11:07:55 +0530

    Add files via upload

---
+++ StringClasses/Example17.java
@@ -0,0 +1,17 @@
+class Example17
+{
+	
+	public static void main(String[] args)
+	{
+		String str="Sahil";
+		//System.out.println(str.length());
+		if (str.length()<7)
+		{
+			System.out.println("String length is sufficient and username is " +str);
+		}
+		else 
+		{
+			System.out.println("String length is no sufficient");			
+		}
+	}
+}
