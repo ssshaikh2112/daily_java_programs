0355f69e8c457bc91acedd73ca06d0df54dc3d6a
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-12 10:54:53 +0530

    Add files via upload

---
+++ StringClasses/Example21.java
@@ -0,0 +1,11 @@
+class Example21
+{
+	public static void main(String[] args)
+	{
+		String name="sahilshaikh";
+		System.out.println(name.substring(5));		// method extract string from main string 
+		System.out.println(name.substring(0,11));
+	}
+}
+//  public java.lang.String substring(int beginIndex);
+//  public java.lang.String substring(int beginIndex, int endIndex);
