d511282c784e1ebb10e8313b8de981fa0e18ecbb
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-12 10:56:01 +0530

    Add files via upload

---
+++ StringClasses/Example24.java
@@ -0,0 +1,12 @@
+class Example24
+{
+	public static void main(String[] args)
+	{
+		String[] str1={"sahil","shaikh","student"};
+	
+		for (int i=0;i<str1.length ;i++ )
+		{
+			System.out.println(str1[i]);
+		}
+	}
+}
