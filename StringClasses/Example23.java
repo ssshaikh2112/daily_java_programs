9377ee88a6d202ca03ec7699b8e3d2d01e4de7a0
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-12 10:55:35 +0530

    Add files via upload

---
+++ StringClasses/Example23.java
@@ -0,0 +1,12 @@
+class Example23
+{
+	public static void main(String[] args)
+	{
+		int[] array={100,200,300,400};
+
+		for (int i=0;i<array.length ;i++ )
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
