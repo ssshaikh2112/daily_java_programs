3b80b4b66c441ccdcd9a1397cb2a35bf93ddd69a
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-14 21:42:38 +0530

    Add files via upload

---
+++ WrapperClasses/Example1.java
@@ -0,0 +1,15 @@
+class Example1
+{
+	public static void main(String[] args)
+	{
+		int a=100;
+		Integer aa=new Integer(a);
+		System.out.println(aa);
+
+		Integer aaa=new Integer("200");
+		System.out.println(aaa);
+	}
+}
+// public java.lang.Integer(int)
+// java.lang.String toString();
+// 
