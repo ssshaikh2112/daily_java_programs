c27849a25e04371aaf9ef0070965b2da0c8d1030
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-14 21:42:58 +0530

    Add files via upload

---
+++ WrapperClasses/Example2.java
@@ -0,0 +1,10 @@
+class Example2
+{
+	public static void main(String[] args)
+	{
+		Integer i=new Integer("100");
+		System.out.println(i);
+		Integer ii=new Integer("Hundread");
+		System.out.println(ii);
+	}
+}
