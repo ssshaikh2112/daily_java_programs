56728e61bb213b1ea4c1d1872768dd4a3fc27652
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-14 21:43:25 +0530

    Add files via upload

---
+++ WrapperClasses/Example3.java
@@ -0,0 +1,14 @@
+class Example3
+{
+	public static void main(String[] args)
+	{
+		Float f1=new Float(202.5);
+		System.out.println(f1);
+		Float f2=new Float(202.5f);
+		System.out.println(f1);
+		Float f3=new Float("202.5");
+		System.out.println(f1);
+		Character c=new Character('A');
+		System.out.println(c);
+	}
+}
