130fd0f5cffd8ad83a85e3ea7401175ca9cbd8d0
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-15 13:34:49 +0530

    Add files via upload

---
+++ WrapperClasses/Example5.java
@@ -0,0 +1,16 @@
+class Example5
+{
+	public static void main(String[] args)
+	{
+		Integer aa=Integer.valueOf(100);
+		System.out.println(aa);
+		System.out.println(aa.toString());
+	}
+}
+/*
+	How many ways are there to convert the primitive data to wrpper object?
+
+	ANS: Two ways:
+				 ie. constructor base approach
+				 	 valueOf() -- it is static and accept primitive data and return type of object.
+*/
