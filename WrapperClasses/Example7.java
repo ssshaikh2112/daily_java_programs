63eb064481d2a65840276f5901f9fc7a4d34399a
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-15 13:35:26 +0530

    Add files via upload

---
+++ WrapperClasses/Example7.java
@@ -0,0 +1,13 @@
+class Example7
+{
+	public static void main(String[] args)
+	{
+		String str1="10";					// string value
+		String str2="20";					// string value
+		int num1=Integer.parseInt(str1);	// convert string to integer
+		int num2=Integer.parseInt(str2);	// convert string to integer
+		System.out.println(num1);			// int value
+		System.out.println(num2);			// int value
+		System.out.println(num1+num2);		// 30
+	}
+}
