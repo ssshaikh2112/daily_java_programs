a829a7538b35b568d1bd464067d6c8ce1eff3343
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-15 13:35:52 +0530

    Add files via upload

---
+++ WrapperClasses/Example8.java
@@ -0,0 +1,13 @@
+class Example8
+{
+	public static void main(String[] args)
+	{
+		Integer number=1000;
+		System.out.println(number);
+		System.out.println(number.toString());
+	}
+}
+/*
+	Directly converting primitive data to wrapper object is Called Boxing.
+	Boxing Operation
+*/
