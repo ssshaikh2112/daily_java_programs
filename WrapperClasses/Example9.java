428fc3b9557115bb8a67750e3c96cd1284a35bbc
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-16 19:33:02 +0530

    Add files via upload

---
+++ WrapperClasses/Example9.java
@@ -0,0 +1,13 @@
+class Example9
+{
+	public static void main(String[] args)
+	{
+		int number=new Integer(1000);
+		System.out.println(number);
+
+	}
+}
+/*
+	Directly converting wrapper object to  primitive data  is Called UnBoxing.
+	UnBoxing Operation
+*/
