a2be96603addc0a9db5b78dd82df765c8a5ff464
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-17 14:56:05 +0530

    Add files via upload

---
+++ Scanner_Classes/Scanner4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Scanner4
+{
+	public static void main(String[] args)
+	{
+
+		Scanner scan= new Scanner(System.in);	//System.in accept input from console
+		while (scan.hasNext())
+		{
+			System.out.println(" "+scan.next());
+		}
+	}
+}
+// public java.io.File(java.lang.String);
