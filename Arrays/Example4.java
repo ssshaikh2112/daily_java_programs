a14fe5c40ab82a615f642691fa25fe4debb5c851
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-19 23:07:37 +0530

    Add files via upload

---
+++ Arrays/Example4.java
@@ -0,0 +1,13 @@
+class Example4
+{
+	public static void main(String[] args)
+	{
+		int[] array = new int[5];
+		System.out.println(array.length);		// length is used for to calculate the length of array.
+
+		for (int i=0;i<array.length ;i++ )
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
