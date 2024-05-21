0fbcfec169234a851534783c0ac4d5667da7c261
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-21 11:24:45 +0530

    Add files via upload

---
+++ Arrays/Example6.java
@@ -0,0 +1,16 @@
+class Example6
+{
+	public static void main(String[] args)
+	{
+		String[] array=new String[3];
+		array[0]="Sahil";
+		array[1]="is a";
+		array[2]="Programmer";
+		for (int i=0;i<=2 ;i++)
+		{
+			System.out.println(array[i]);
+		}
+
+		
+	}
+}
