f80f81cf8ce3faaf4f13161e26a93b8b01e6da1c
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-21 11:25:18 +0530

    Add files via upload

---
+++ Arrays/Example8.java
@@ -0,0 +1,17 @@
+class Example8
+{
+	// for each loop is used to extract object.
+	/*
+		for(store in a variable based on data / object extracted : mention from where to extract array / contain
+
+		It will extract mention data and store in variable And variable should be of same type based on data object extraction.
+	*/
+	public static void main(String[] args)
+	{
+		String[] ss={"Sahil","is a ","Programmer"};
+		for(String s:ss)
+		{
+			System.out.println(s);
+		}
+	}
+}
