12bce48941cc0d9834e7177b368f5b11e032977d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-21 11:25:03 +0530

    Add files via upload

---
+++ Arrays/Example7.java
@@ -0,0 +1,11 @@
+class Example7
+{
+	public static void main(String[] args)
+	{
+		int[] array={10,20,30,40};			// Another type to declare array
+		for (int i=0;i<array.length ;i++ )
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
