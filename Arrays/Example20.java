0decec5257d528a07f12177eb5c3e3418e614924
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-05-27 16:34:51 +0530

    Add files via upload

---
+++ Arrays/Example20.java
@@ -0,0 +1,21 @@
+class Example20
+{
+	public static void main(String[] args)
+	{
+		/*
+				10 20 30
+				40 50 60
+		*/
+		// 2nd approach to declare the Multidimensional array
+		int[][] array={{10,20,30} ,
+						{40,50,60}};
+
+		System.out.println(array[0][1]);
+		System.out.println(array[0][2]);
+		System.out.println(array[1][2]);
+
+
+			
+
+	}
+}
