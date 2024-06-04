9bdbe5831f9891616c296c0d3a7b338532e04153
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-06-04 08:43:21 +0530

    Add files via upload

---
+++ Collections/ArrayList4.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayList4 {
+
+	public static void main(String[] args) {
+		// It can store only homogeneous <Integer>	--> <> is called canonical item
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		al.add(100);
+		al.add(200);
+		al.add(300);
+		System.out.println(al);
+			
+		//al.add("sahil");
+		//System.out.println(al);
+	}
+
+}
