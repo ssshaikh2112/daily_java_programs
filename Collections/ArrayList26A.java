a85ec0a5d003f1f46bcea58bae058d855385fff5
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-06-19 11:11:56 +0530

    Add files via upload

---
+++ Collections/ArrayList26A.java
@@ -0,0 +1,30 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayList26A {
+
+	public static void main(String[] args) {
+ArrayList<SwiggyItemIdComparable> al=new ArrayList<SwiggyItemIdComparable>();
+		
+		al.add(new SwiggyItemIdComparable(101,"pizza"));
+		al.add(new SwiggyItemIdComparable(401,"burger"));
+		al.add(new SwiggyItemIdComparable(301,"paneer"));
+		al.add(new SwiggyItemIdComparable(201,"nan"));
+		
+		System.out.println(al);
+		for(SwiggyItemIdComparable snc : al)
+		{
+			System.out.println(snc.itemId+" "+snc.itemName);
+		}
+		System.out.println("----------");
+		Collections.sort(al);	// Sort alphabetically
+		
+		for(SwiggyItemIdComparable snc : al)
+		{
+			System.out.println(snc.itemId+" "+snc.itemName);
+		}
+	}
+
+}
