dde9b3bbed6359a2f0568320203043816043598c
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-06-20 09:15:05 +0530

    Add files via upload

---
+++ Collections/Amazon.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class Amazon 
+{
+	int productId;
+	String productName;
+	public Amazon(int productId, String productName) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+	}
+	
+
+}
