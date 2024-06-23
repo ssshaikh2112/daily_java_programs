7c11b5a161ef912c5caf0b8d3ae887c2ff5e6b0a
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-06-23 14:12:31 +0530

    Add files via upload

---
+++ Collections/Employee.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class Employee {
+	int empId;
+	String empName;
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+
+}
