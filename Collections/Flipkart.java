ca96db39310c815eb70d96d74262930b134fc6a0
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-06-24 09:20:23 +0530

    Add files via upload

---
+++ Collections/Flipkart.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class Flipkart
+{
+
+	int productId;
+	String productName;
+	public Flipkart(int productId, String productName) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+	}
+	
+}
