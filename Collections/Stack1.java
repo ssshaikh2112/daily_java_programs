89fca2c0094335e6150c54d13722a72486fb311d
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-07-05 20:14:30 +0530

    Add files via upload

---
+++ Collections/Stack1.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.Stack;
+
+public class Stack1 {
+
+	public static void main(String[] args) 
+	{
+		Stack stack=new Stack();
+		stack.push(101);
+		stack.push("oggy");
+		stack.push("entertainment");
+		stack.push(102.21);
+		System.out.println(stack);
+	}
+
+}
