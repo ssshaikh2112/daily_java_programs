05f46cb34482aabca38c6d6c4644400d3eab5678
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-07-18 17:31:04 +0530

    Update TreeMap5.java

--- Collections/TreeMap5.java
+++ Collections/TreeMap5.java
@@ -4,7 +4,8 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class TreeMap5 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		TreeMap tm=new TreeMap();
 		tm.put(103, "oggy");
 		tm.put(102, "jack");
