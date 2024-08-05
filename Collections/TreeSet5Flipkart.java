ce2ab121092418cc190fed2e59b1650c32b61863
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-08-05 10:14:16 +0530

    Add files via upload

---
+++ Collections/TreeSet5Flipkart.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class TreeSet5Flipkart {
+	int productId;
+	String productName;
+	public TreeSet5Flipkart(int productId, String productName) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+	}
+	
+
+}
