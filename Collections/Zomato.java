39c314da3af4910b33d1e068308ad354b94220d6
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2024-08-14 13:55:47 +0530

    Add files via upload

---
+++ Collections/Zomato.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class Zomato 
+{
+	int itemId;
+	String itemName;
+	public Zomato(int itemId , String itemName) 
+	{
+		this.itemId=itemId;
+		this.itemName=itemName;
+	}
+
+}
