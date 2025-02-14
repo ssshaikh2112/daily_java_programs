669efd42da43f56c4ad30dbcaad63218b57da1d6
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-02-14 22:27:01 +0530

    Update Byte1.java

--- Byte1.java
+++ Byte1.java
@@ -3,6 +3,7 @@ class Byte1
 	public static void main(String [] args)
 	{
 		byte var1=10;
+		
 		System.out.println(var1);
 		
 		//byte var2=128;	Generate error, ANS : When assign a value in integral data type then by default it is integer.
