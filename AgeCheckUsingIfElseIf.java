c78503b43e5510bf11a809754716d1fa3776ade1
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-06-04 15:10:22 +0530

    Update AgeCheckUsingIfElseIf.java

--- AgeCheckUsingIfElseIf.java
+++ AgeCheckUsingIfElseIf.java
@@ -8,7 +8,6 @@ class AgeCheckUsingIfElseIf
 		if(age>=21)
 		{
 			System.out.println("Age is Matching...");
-			
 			if(gender=='M' || gender=='F')
 			{
 				System.out.println("Gender is Matching...");
