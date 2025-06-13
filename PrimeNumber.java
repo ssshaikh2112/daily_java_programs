c85204daa32d33bb4f99aa36ed9afc0d9c6bc5da
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-06-13 23:55:17 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -7,6 +7,8 @@ class PrimeNumber
 		int number =8;
 		int temp = 0;						 // Temporary variable used and assign the value to avoid the printing of repesting statement in for loop
 
+
+		
 		for(int i =2; i<=number-1; i++)		// Number is greater than 1 (prime number divisible by 1 and itself so i start from 2 till number - 1)
 		{
 			if(number%i==0)					// If any number is divided by the between any number so it is not prime number
