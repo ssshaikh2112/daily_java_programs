31357b7097202b8b77163c5883fa4adbce0a688b
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-06-17 23:40:58 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -4,6 +4,7 @@ class StarPattern
 	{
 		for(int column=1;column<=5;column++)		// encounter the logic for column where 1st column * star , 2nd column * * , etc
 		{
+			
 			for(int row=1;row<=column;row++)	// encounter the logic for row where 1st row * star , 2nd row * * , etc
 			{
 				System.out.print(" * ");		// use only print because print all * in one line
