a853d8ada0641cf6ee76b22866ebfa28612d7e7b
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-09-02 23:03:14 +0530

    Update ForloopCheck.java

--- ForloopCheck.java
+++ ForloopCheck.java
@@ -3,8 +3,6 @@ class ForloopCheck
 
 	public static void main(String args[])
 	{
-
-
 		
 		int variable = 5;
 		System.out.println(variable);		// print original value	[5]
@@ -14,6 +12,7 @@ class ForloopCheck
 		System.out.println(variable);		// Print Incremented value	[7]
 	}
 }
+
 
 
 
